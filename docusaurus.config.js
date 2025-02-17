4e7eae4922838fbe57f4a7c799c502608e39be81
Roy Russo <royrusso@gmail.com>
2025-02-17 14:57:29 -0500

    added gtag support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,6 +75,10 @@ const config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        gtag: {
+          trackingID: "G-6PB3YWX6RW",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
